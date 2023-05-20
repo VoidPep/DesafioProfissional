d74ffdd8684a426c98f058b325e4220afbcba83e
Void Pep <pedrohm1009@gmail.com>
2023-05-20 18:41:48 -0300

    adição do BeanValidation na tabela de funcionarios

--- api/src/main/java/com/wastewizard/api/funcionario/Endereco.java
+++ api/src/main/java/com/wastewizard/api/funcionario/Endereco.java
@@ -1,6 +1,8 @@
 package com.wastewizard.api.funcionario;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,11 +13,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Endereco {
+    @NotBlank
     private String logradouro;
+    @NotBlank
     private String bairro;
+    @NotBlank @Pattern(regexp = "\\d{9}")
     private String cep;
+    @NotBlank
     private String numero;
+    @NotBlank
     private String complemento;
+    @NotBlank
     private String cidade;
+    @NotBlank
     private String uf;
 }
--- api/src/main/java/com/wastewizard/api/funcionario/FuncionarioRequest.java
+++ api/src/main/java/com/wastewizard/api/funcionario/FuncionarioRequest.java
@@ -1,15 +1,28 @@
 package com.wastewizard.api.funcionario;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record FuncionarioRequest(
+        @NotBlank
         String nome,
+        @NotBlank
         String cpf,
+        @NotBlank @Email
         String email,
+        @NotBlank
         String entrada,
+        @NotBlank
         String saida,
+        @NotBlank
         BigDecimal salario,
+        @NotBlank
         String pis,
+        @NotNull @Valid
         Endereco endereco)
 {
 }
